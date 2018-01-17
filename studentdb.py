c4647ab67508c8aa24e1c70d7fbdf2a4737b7348
Ronit <ronit10z@gmail.com>
2018-01-16 20:24:40 -0500

    mess

--- studentdb.py
+++ studentdb.py
@@ -1,25 +1,79 @@
 from kivy.app import App
+from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.listview import ListItemButton
- 
- 
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.uix.popup import Popup
+from collections import OrderedDict
+
+import csv
+import os
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
 class StudentListButton(ListItemButton):
     pass
  
- 
+
 class StudentDB(BoxLayout):
  
     # Connects the value in the TextInput widget to these
     # fields
-    first_name_text_input = ObjectProperty()
-    last_name_text_input = ObjectProperty()
+    name_text_input = ObjectProperty()
     student_list = ObjectProperty()
- 
+
+    loadfile = ObjectProperty()
+    savefile = ObjectProperty()
+    text_input = ObjectProperty()
+
+    
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        assert(filename[0].endswith(".csv"))
+
+        with open(os.path.join(path, filename[0])) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            names = [row[0] for row in readCSV]
+            for name in names:
+                self.student_list.adapter.data.extend([name])
+
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+
+        self.dismiss_popup()
+
     def submit_student(self):
  
         # Get the student name from the TextInputs
-        student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+        student_name = self.name_text_input.text
  
         # Add the student to the ListView
         self.student_list.adapter.data.extend([student_name])
@@ -65,8 +119,13 @@ class StudentDB(BoxLayout):
 class StudentDBApp(App):
     def build(self):
         return StudentDB()
- 
- 
-dbApp = StudentDBApp()
- 
-dbApp.run()
+
+    
+
+def main():
+    dbApp = StudentDBApp()
+    dbApp.run()
+    return
+
+if __name__ == '__main__':
+    main()

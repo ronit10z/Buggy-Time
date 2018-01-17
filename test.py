f79702031287c363560106cd551ad91d3f279cb5
Ronit <ronit10z@gmail.com>
2018-01-17 11:21:49 -0500

    work:

--- test.py
+++ test.py
@@ -1,119 +1,180 @@
 import tkinter as tk
+import Tkconstants, tkFileDialog
+import csv
+from collections import defaultdict
+
+
+def openFile():
+
+	fileName =  tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",
+		filetypes = (("csv files","*.csv"),("all files","*.*")))
+	return fileName
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    getFld = tk.IntVar()
-    root.wm_title("Poop")
-    root.iconbitmap("poop.ico")
+		root = tk.Tk()
+		getFld = tk.IntVar()
+		root.wm_title("Poop")
+		root.iconbitmap("poop.ico")
+
+		nameTimesDict = defaultdict(list)
 
 #                                   MENU
 #******************************************************************************
-    def hello():
-        print ("hello!")
-    menubar = tk.Menu(root)
+		def hello():
+				print ("hello!")
+		menubar = tk.Menu(root)
 
-    # create a pulldown menu, and add it to the menu bar
-    filemenu = tk.Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Open", command=hello)
-    filemenu.add_command(label="Save", command=hello)
-    filemenu.add_separator()
-    filemenu.add_command(label="Exit", command=root.quit)
-    menubar.add_cascade(label="File", menu=filemenu)
+		# create a pulldown menu, and add it to the menu bar
+		filemenu = tk.Menu(menubar, tearoff=0)
+		filemenu.add_command(label="Open", command=openFile)
+		filemenu.add_command(label="Save", command=hello)
+		filemenu.add_separator()
+		filemenu.add_command(label="Exit", command=root.quit)
+		menubar.add_cascade(label="File", menu=filemenu)
 
-    # create more pulldown menus
-    editmenu = tk.Menu(menubar, tearoff=0)
-    editmenu.add_command(label="Cut", command=hello)
-    editmenu.add_command(label="Copy", command=hello)
-    editmenu.add_command(label="Paste", command=hello)
-    menubar.add_cascade(label="Edit", menu=editmenu)
+		# create more pulldown menus
+		editmenu = tk.Menu(menubar, tearoff=0)
+		editmenu.add_command(label="Cut", command=hello)
+		editmenu.add_command(label="Copy", command=hello)
+		editmenu.add_command(label="Paste", command=hello)
+		menubar.add_cascade(label="Edit", menu=editmenu)
 
-    helpmenu = tk.Menu(menubar, tearoff=0)
-    helpmenu.add_command(label="About", command=hello)
-    menubar.add_cascade(label="Help", menu=helpmenu)
+		helpmenu = tk.Menu(menubar, tearoff=0)
+		helpmenu.add_command(label="About", command=hello)
+		menubar.add_cascade(label="Help", menu=helpmenu)
 
-    # display the menu
-    root.config(menu=menubar)
+		# display the menu
+		root.config(menu=menubar)
 
 #                                   TABLE
 #******************************************************************************
-    tableLf = tk.LabelFrame(root, text=" Pusher Times ")
-    tableLf.grid(row=0, column=9, columnspan=2, rowspan=8, \
-                sticky='NS', padx=5, pady=5)
-    tableCanvas = tk.Canvas(tableLf)
-    tableCanvas.grid(row=0)
+		tableLf = tk.LabelFrame(root, text=" Pusher Times ")
+		tableLf.grid(row=0, column=9, columnspan=2, rowspan=8, \
+								sticky='NS', padx=5, pady=5)
+		tableCanvas = tk.Canvas(tableLf)
+		tableCanvas.grid(row=0)
 
-    tableFrame = tk.Frame(tableCanvas)
-    tableFrame.grid(row=0, column=0, sticky='nw')
+		tableFrame = tk.Frame(tableCanvas)
+		tableFrame.grid(row=0, column=0, sticky='nw')
 
-    Can1 = tk.Canvas(tableFrame)
-    Can1.grid(row=0, column=0)
+		Can1 = tk.Canvas(tableFrame)
+		Can1.grid(row=0, column=0)
 
-    vsbar = tk.Scrollbar(tableFrame, orient="vertical", command=Can1.yview)
-    vsbar.grid(row=0, column=1, sticky='ns')
-    Can1.configure(yscrollcommand=vsbar.set)
+		vsbar = tk.Scrollbar(tableFrame, orient="vertical", command=Can1.yview)
+		vsbar.grid(row=0, column=1, sticky='ns')
+		Can1.configure(yscrollcommand=vsbar.set)
 
-    hsbar = tk.Scrollbar(tableFrame, orient="horizontal", command=Can1.xview)
-    hsbar.grid(row=1, column=0, sticky='ew')
-    Can1.configure(xscrollcommand=hsbar.set)
+		hsbar = tk.Scrollbar(tableFrame, orient="horizontal", command=Can1.xview)
+		hsbar.grid(row=1, column=0, sticky='ew')
+		Can1.configure(xscrollcommand=hsbar.set)
 
-    # Create a frame to contain the buttons
-    frame_buttons = tk.Frame(Can1, bd=2, relief=tk.GROOVE)
-    Can1.create_window((0,0), window=frame_buttons,anchor='nw')
+		# Create a frame to contain the buttons
+		frame_buttons = tk.Frame(Can1, bd=2, relief=tk.GROOVE)
+		Can1.create_window((0,0), window=frame_buttons,anchor='nw')
 
-    # Bind the buttons frame to a function that fixes the Canvas size
-    def resize(event):
-        Can1.configure(scrollregion=Can1.bbox("all"), width=235, height=300)
-    frame_buttons.bind("<Configure>", resize)
+		# Bind the buttons frame to a function that fixes the Canvas size
+		def resize(event):
+				Can1.configure(scrollregion=Can1.bbox("all"), width=235, height=300)
+		frame_buttons.bind("<Configure>", resize)
 
-    # Add the buttons to the frame
-    rows = 50
-    columns = 50
-    names = ["Alice","Bob","Charlie","David","Eve","Frank","Grace","Heidi","Isabella","Judy","Kevin","Lucy","Mallory","Nicole","Oscar","Pat","Richard","Sybil","Trent","Victor","Wendy"]
-    for i in range(0,rows):
-        for j in range(0,columns):
-            entry = tk.Entry(frame_buttons, text="", width=8)
-            if (j == 0 and i < len(names)):
-                entry.configure(width=15)
-                entry.delete(0,tk.END)      
-                entry.insert(0, names[i])
-            entry.grid(row=i, column=j, sticky='news')
+		# Add the buttons to the frame
+		rows = 50
+		columns = 10
+		# names = ["Alice","Bob","Charlie","David","Eve","Frank","Grace","Heidi","Isabella","Judy","Kevin","Lucy","Mallory","Nicole","Oscar","Pat","Richard","Sybil","Trent","Victor","Wendy"]
+		# for i in range(0,rows):
+		#     for j in range(0,columns):
+		#         entry = tk.Entry(frame_buttons, text="", width=8)
+		#         if (j == 0 and i < len(names)):
+		#             entry.configure(width=15)
+		#             entry.delete(0,tk.END)      
+		#             entry.insert(0, names[i])
+		#         entry.grid(row=i, column=j, sticky='news')
+
+		nameTimesDict["Kevin"].append(13.2)
+		nameTimesDict["James"].append(14.1)
+		dictKeys = sorted(nameTimesDict.keys())
+		print(dictKeys)
+		entryGrid = [[0]*columns for row in range(rows)]
+		print(entryGrid)
+
+		for i in range(0,rows):
+			if (i < len(dictKeys)):
+				currentName = dictKeys[i]
+				currentTimes = nameTimesDict[currentName]
+				print(currentTimes)
+			else:
+				currentName = None
+				currentTimes = None
+
+			for j in range(0,columns):
+					entry = tk.Entry(frame_buttons, text="", width=8)
+					if (j == 0 and currentName != None):
+							entry.configure(width=15)
+							entry.delete(0,tk.END)
+							entry.insert(0, currentName)
+					elif (currentTimes != None and j < len(currentTimes) + 1):
+							print(currentTimes)
+							entry.configure(width=8)
+							entry.delete(0,tk.END)      
+							entry.insert(0, currentTimes[j - 1])
+					entry.grid(row=i, column=j, sticky='news')
+
+		
+
 
 #                              TIMER CONTROL
 #******************************************************************************
-    def color_change():
-        goButton.configure(bg = "gray85")
+		def color_change():
+				goButton.configure(bg = "gray85")
 
-    goButton = tk.Button(root, text="GO", font=("Helvetica", 16), bg = "lawn green", command = color_change)
-    goButton.grid(row=6, columnspan=4, sticky='NSEW', \
-                 padx=5, pady=5, ipadx=5, ipady=5)
-    
-    timerLF = tk.LabelFrame(root, text=" Time: ")
-    timerLF.grid(row=5, column=1, columnspan=2, sticky='EW', \
-                 padx=5, pady=5, ipadx=5, ipady=5)
-    
-    timer = tk.Label(timerLF, text= "00:00",font=("Helvetica", 16))
-    timer.grid(sticky='EW', padx=5, pady=2)
+		goButton = tk.Button(root, text="GO", font=("Helvetica", 16), bg = "lawn green", command = color_change)
+		goButton.grid(row=6, columnspan=4, sticky='NSEW', \
+								 padx=5, pady=5, ipadx=5, ipady=5)
+		
+		timerLF = tk.LabelFrame(root, text=" Time: ")
+		timerLF.grid(row=5, column=1, columnspan=2, sticky='EW', \
+								 padx=5, pady=5, ipadx=5, ipady=5)
+		
+		timer = tk.Label(timerLF, text= "00:00",font=("Helvetica", 16))
+		timer.grid(sticky='EW', padx=5, pady=2)
 
 
 
 #                            FILE MANAGEMENT
 #******************************************************************************
+		
+		fileLF = tk.LabelFrame(root, text=" CSV File ")
+		fileLF.grid(row=2, columnspan=4, sticky='W', \
+									 padx=5, pady=5, ipadx=5, ipady=5)
 
-    fileLF = tk.LabelFrame(root, text=" CSV File ")
-    fileLF.grid(row=2, columnspan=4, sticky='W', \
-                   padx=5, pady=5, ipadx=5, ipady=5)
+		fileTextBox = tk.Entry(fileLF)
+		fileTextBox.grid(row=0, column=0, sticky='WE', padx=5, pady=5)
+
+		def browseFile():
+			fileName = openFile()
+			fileTextBox.insert(0, fileName)
+
+		browse = tk.Button(fileLF, text=" Browse... ", command=browseFile)
+		browse.grid(row=0, column=3, columnspan=2, sticky='W', padx=5, pady=2)
 
 
-    browse = tk.Button(fileLF, text=" Browse... ")
-    browse.grid(row=0, column=3, columnspan=2, sticky='W', padx=5, pady=2)
+		def inportFile():
+			fileName = fileTextBox.get()
+			if (fileName.endswith(".csv")):
+					with open(fileName) as csvfile:
+						readCSV = csv.reader(csvfile, delimiter=',')
 
-    fileTextBox = tk.Entry(fileLF)
-    fileTextBox.grid(row=0, column=0, sticky='WE', padx=5, pady=5)
 
-    inport = tk.Button(fileLF, text=" Import ")
-    inport.grid(row=1, column=0, columnspan=2, sticky='EW', padx=5, pady=2)
+		inport = tk.Button(fileLF, text=" Import ", command= inportFile)
+		inport.grid(row=1, column=0, columnspan=2, sticky='EW', padx=5, pady=2)
 
-    export = tk.Button(fileLF, text=" Export ")
-    export.grid(row=2, column=0, columnspan=2, sticky='EW', padx=5, pady=2)
+		def updateCell(name, value):
+			nameTimesDict[name].append(value)
 
-    root.mainloop()
+			row = nameTimesDict.index(name)
+			col = len(nameTimesDict[name]) + 1
+
+		export = tk.Button(fileLF, text=" Export ")
+		export.grid(row=2, column=0, columnspan=2, sticky='EW', padx=5, pady=2)
+
+		root.mainloop()
--- tkinterMenu.py
+++ tkinterMenu.py
@@ -1,23 +1,122 @@
-import sys
 from Tkinter import *
+import tkinter as tk
+import Tkconstants, tkFileDialog
+import tkMessageBox
+from coordinator import *
+import csv
 
+
+names = ["Kevin"]
 root = Tk()
+root.minsize(width=500, height=500)
+root.columnconfigure(0, weight=1)
 
-# Insert a menu bar on the main window
-menubar = Menu(root)
-root.config(menu=menubar)
+canvas = None
+frame = None
+ 
+def donothing():
+	 x = 0
+ 
+def openFile():
+	global names
+	fileName =  tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",
+		filetypes = (("csv files","*.csv"),("all files","*.*")))
+	with open(fileName) as csvfile:
+		readCSV = csv.reader(csvfile, delimiter=',')
+		names = [row[0] for row in readCSV]
+	drawSpreadSheet()
 
-# Create a menu button labeled "File" that brings up a menu
-filemenu = Menu(menubar)
-menubar.add_cascade(label='File', menu=filemenu)
+def drawSpreadSheet():
+	width = 5
+	for i in range(len(names)): #Rows
+		for j in range(width): #Columns
+			if (j == 0):
+				b = Entry(frame, text="")
+				b.grid(row=i, column=j)
+				b.insert(0, names[i])
+			else:
+				b = Entry(frame, text="")
+				b.grid(row=i, column=j)
 
-# Create entries in the "File" menu
-# simulated command functions that we want to invoke from our menus
-def doPrint(  ): print 'doPrint'
-def doSave(  ): print 'doSave'
-filemenu.add_command(label='Print', command=doPrint)
-filemenu.add_command(label='Save', command=doSave)
-filemenu.add_separator(  )
-filemenu.add_command(label='Quit', command=sys.exit)
+def clearSpreadSheet():
+	for i in range(len(names)): #Rows
+		for j in range(width): #Columns
+			if (j == 0):
+				b = Entry(frame, text="")
+				b.grid(row=i, column=j)
+				b.insert(0, names[i])
+			else:
+				b = Entry(frame, text="")
+				b.grid(row=i, column=j)
 
-root.mainloop()
+
+
+def on_configure(event):
+	# update scrollregion after starting 'mainloop'
+	# when all widgets are in canvas
+	canvas.configure(scrollregion=canvas.bbox('all'))
+
+def helloCallBack():
+   tkMessageBox.showinfo( "Arduino Time", "Ping Time" + str(TakeReading()))
+		 
+def main():
+	global canvas
+	global frame
+	print("loop")
+
+	menubar = Menu(root)
+	filemenu = Menu(menubar, tearoff=0)
+	filemenu.add_command(label="New", command=donothing)
+	filemenu.add_command(label="Open", command=openFile)
+	filemenu.add_command(label="Save", command=donothing)
+	filemenu.add_command(label="Clear", command=clearSpreadSheet)
+
+	filemenu.add_separator()
+	filemenu.add_command(label="Exit", command=root.quit)
+	menubar.add_cascade(label="File", menu=filemenu)
+	 
+	helpmenu = Menu(menubar, tearoff=0)
+	helpmenu.add_command(label="Help Index", command=donothing)
+	helpmenu.add_command(label="About...", command=donothing)
+	menubar.add_cascade(label="Help", menu=helpmenu)
+	
+
+	root.config(menu=menubar)
+	canvas = tk.Canvas(root)
+
+	
+
+	# update scrollregion after starting 'mainloop'
+	# when all widgets are in canvas
+	canvas.bind('<Configure>', on_configure)
+
+	# --- put frame in canvas ---
+
+	frame = Frame(canvas)
+	canvas.create_window((0,0), window=frame, anchor='nw')
+
+	scrollbar = Scrollbar(root, command=canvas.xview, orient=HORIZONTAL)
+	scrollbar.pack(side=BOTTOM, fill='x')
+	canvas.configure(xscrollcommand = scrollbar.set)
+
+	scrollbar = Scrollbar(root, command=canvas.yview)
+	scrollbar.pack(side=RIGHT, fill='y')
+	canvas.configure(yscrollcommand = scrollbar.set)
+
+	canvas.pack(side=tk.LEFT)
+
+
+	readyButton = Button(root, text ="Ready", command = helloCallBack)
+	readyButton.pack()
+
+	benchMark = Button(root, text ="benchMark", command = donothing)
+	benchMark.pack()
+
+	drawSpreadSheet()
+	
+	root.mainloop()
+
+
+if __name__ == '__main__':
+	main()
+ 
